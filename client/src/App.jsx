9a5ada466de965747c15ca5983daf1e1410da6bb
Ashishsahu0107 <ashishsahu01072005@gmail.com>
2026-08-24 23:24:35 +0530

    feat: add new landing pages for Jobs and MediaSharing, and integrate them into the main application router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
+import Jobs from "./pages/Jobs";
+import MediaSharing from "./pages/MediaSharing";
 import Chat from "./pages/Chat";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -20,6 +22,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact/>}/>
+        <Route path="/jobs" element={<Jobs/>}/>
+        <Route path="/media-sharing" element={<MediaSharing/>}/>
         <Route path="/settings" element={authUser ? <Settings/> : <Navigate to="/login" />}/>
         <Route path="/chat" element={authUser ? <Chat/> : <Navigate to="/login" />}/>
         <Route path="/login" element={!authUser ? <Login/> : <Navigate to="/chat" />}/>
