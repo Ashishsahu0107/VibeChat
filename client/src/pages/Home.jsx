9a001ee824eb96eb4c3a8fa0c453b1ba79c0769b
Ashishsahu0107 <ashishsahu01072005@gmail.com>
2026-08-24 23:24:43 +0530

    feat: add Home, Jobs, and MediaSharing landing pages to client frontend

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -277,13 +277,13 @@ const Home = () => {
           <h6 className="footer-title">Services</h6> 
           <a className="link link-hover cursor-pointer">Direct Messaging</a>
           <a className="link link-hover cursor-pointer">Group Chats</a>
-          <a className="link link-hover cursor-pointer">Media Sharing</a>
+          <Link to="/media-sharing" className="link link-hover">Media Sharing</Link>
         </nav> 
         <nav>
           <h6 className="footer-title">Company</h6> 
           <Link to="/" className="link link-hover">About us</Link>
           <Link to="/contact" className="link link-hover">Contact</Link>
-          <a className="link link-hover cursor-pointer">Jobs</a>
+          <Link to="/jobs" className="link link-hover">Jobs</Link>
         </nav> 
         <nav>
           <h6 className="footer-title">Legal</h6> 
